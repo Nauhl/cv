4b68fe65333622bd4e0f4cc846e0d4013a40f619
Horacio Zorola <73261219+horaciozoroladev@users.noreply.github.com>
2023-02-16 22:56:28 -0600

    Updates

---
+++ assets/data.js
@@ -0,0 +1,126 @@
+export let data = {
+    es: {
+        titulo: "Hola, soy",
+        nombreCorto: "Horacio",
+        profesion: "Fullstack Developer",
+        acerca: {
+            de: "Acerca de",
+            mi: "Mi",
+            contenido: `<p>Hola <span class="wave">👋</span>, Mi nombre es
+            <span class="t__uva">Horacio Zorola</span> y soy
+            <span class="t__bold t__uva">Ingeniero en Software 🧑🏽‍💻</span> orientado a soluciones principalmente web con más
+            de 4 años de experiencia en el área.</p>`
+        },
+        techStack: {
+            tech: 'Tech',
+            stack: 'Stack',
+            contenido: `<p>
+			Mi stack tecnológico principal está conformado por
+			<span class="t__uva">JavaScript, TypeScript,</span> y sus frameworks
+			<span class="t__uva">Angular, Svelte</span> y
+			<span class="t__uva"> Node.js</span> pero no me limito solo a estos lenguajes y sus herramientas, ya que trabajo comodamente con
+			<span class="t__uva">GitHub, PHP, MySQL, Java, Sharepoint y Miro</span> para diagramas.
+		    </p>`
+        },
+        experiencia: {
+            mi: "Mi",
+            experiencia: "Experiencia",
+            descripcion: "En mi carrera, he tenido la oportunidad de trabajar en una amplia gama de proyectos, desde aplicaciones web a gran escala hasta pequeñas herramientas de utilidad, y siempre he disfrutado el desafío de encontrar soluciones creativas a problemas complejos. Si bien me tomo mi trabajo en serio, también creo en divertirme y aportar sentido del humor a todo lo que hago.",
+            empresas: [
+                {
+                    titulo: "Desarrollador Web Jr",
+                    logo: "",
+                    backgroundColor: "#fe628f",
+                    empresa: "Vanuva Design Agency",
+                    tiempo: "Febrero 2018 - Septiembre 2018",
+                    descripcion: "Trabajé con JavaScript, PHP para desarrollar páginas web; coorporativas y landing page. Haciendo uso de servidores para alojar dichas páginas y de aplicaciones FTP tal como Filezilla para transferir archivos.",
+                },
+                {
+                    titulo: "Desarrollador Fullstack",
+                    logo: "/cv/assets/imgs/pbs.png",
+                    backgroundColor: "",
+                    empresa: "Proper Business Solutions",
+                    tiempo: "Agosto 2020 - Octubre 2021",
+                    descripcion: `
+                    Trabajé en diferentes proyectos y plataformas, realizando tareas que incluyeron mantenimiento y creación de nuevas plataformas utilizando en su entonces las últimas versiones de Angular y servicios de Node.js.
+                    Plataforma de multitenencia en Angular y APIs desarrolladas utilizando la arquitectura de microservicios en Node.js, integrando actualizaciones y refactorizando la apariencia del sitio web, lanzando con éxito una versión inicial. Crecí como persona y aprendí tecnologías emergentes mientras trabajaba en equipo.`,
+                },
+                {
+                    titulo: "Desarrollador Frontend",
+                    logo: "/cv/assets/imgs/itesm.png",
+                    backgroundColor: "",
+                    empresa: "Tecnológico de Monterrey",
+                    tiempo: "Diciembre 2021 - Actual",
+                    descripcion: `Siendo parte de un equipo multidiciplinario fuí participe de una gama de proyectos, en el portal de proveedores. Desarrollé un servicio interno en el área filantrópica usando Angular 14 y Material UI, haciendo la experiencia de usuario mejorada al poner la plataforma en producción. Implementación de gran impacto en las audiencias actuales y futuras de usuarios para la platafora principal.`,
+                }
+            ]
+        },
+        contacto: {
+            contacta: 'Contáctame',
+            me: '',
+            contenido: 'Espero que este sitio web le brinde una idea de quién soy como desarrollador y persona, y espero conectarme con usted y aprender más sobre sus proyectos e intereses. ¡Gracias por su visita!'
+        }
+    },
+    en: {
+        titulo: "Hi, I'm",
+        nombreCorto: "Horacio",
+        profesion: "Fullstack Developer",
+        acerca: {
+            de: "About",
+            mi: "Me",
+            contenido: `<p>Hi <span class="wave">👋</span>, My name is
+            <span class="t__uva">Martin</span> and I am a
+            <span class="t__bold t__uva">Software Engineer 🧑🏽‍💻</span> oriented to mainly web solutions with more than 3 years of experience in freelance and startup.</p>`
+        },
+        techStack: {
+            tech: 'Tech',
+            stack: 'Stack',
+            contenido: `<p>
+			My main technology stack is conformed by
+			<span class="t__uva">JavaScript, Typescript,</span> and
+			their frameworks
+			<span class="t__uva">Angular, Svelte</span> and
+			<span class="t__uva"> Node.js</span> but I don't limit myself to just
+			these languages ​​and their tools, since I work comfortably with
+			<span class="t__uva">GitHub, PHP, MySQL, Java, Sharepoint and Miro</span> for diagrams. 
+		    </p>`
+        },
+        experiencia: {
+            mi: "My",
+            experiencia: "Experience",
+            descripcion: "In my career, I have had the opportunity to work on a wide range of projects, from large-scale web applications to small utility tools, and I have always enjoyed the challenge of finding creative solutions to complex problems. While I take my work seriously, I also believe in having fun and bringing a sense of humor to everything I do.",
+            empresas: [
+                {
+                    titulo: "Web Developer Jr",
+                    logo: "",
+                    backgroundColor: "#fe628f",
+                    empresa: "Vanuva Design Agency",
+                    tiempo: "February 2018 - September 2018",
+                    descripcion: "I worked with JavaScript, PHP to develop web pages; corporate and landing page. Making use of servers to host said pages and FTP applications such as Filezilla to transfer files.",
+                },
+                {
+                    titulo: "Fullstack Developer",
+                    logo: "/cv/assets/imgs/pbs.png",
+                    backgroundColor: "",
+                    empresa: "Proper Business Solutions",
+                    tiempo: "August 2020 - October 2021",
+                    descripcion: `
+                    I worked on different projects and platforms, performing tasks that included maintenance and creation of new platforms using the latest versions of Angular and Node.js services. Multi-tenant platform in Angular and APIs developed using the microservices architecture in Node.js, integrating updates and refactoring the appearance of the website, successfully launching an initial version. I grew as a person and learned emerging technologies while working as a team.`,
+                },
+                {
+                    titulo: "Frontend Developer",
+                    logo: "/cv/assets/imgs/itesm.png",
+                    backgroundColor: "",
+                    empresa: "Tecnológico de Monterrey",
+                    tiempo: "December 2021 - Now",
+                    descripcion: `Being part of a multidisciplinary team, I participated in a range of projects, in the supplier portal. I developed an internal service in the philanthropic area using Angular 14 and Material UI, making the user experience improved by putting the platform into production. Implementation of great impact in the current and future audiences of users for the main platform.`,
+                }
+            ]
+        },
+        contacto: {
+            contacta: 'Contact',
+            me: 'me',
+            contenido: 'I hope this website gives you a sense of who I am as a developer and a person, and I look forward to connecting with you and learning more about your projects and interests. Thank you for visiting!'
+        }
+    }
+}
